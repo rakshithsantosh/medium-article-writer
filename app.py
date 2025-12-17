8b50df762175040bb6bd7d697a1dff5b45b5d7c2
rakshith_s <rakshithsantosh@gmail.com>
2025-12-17 20:18:43 +0530

    add websearch agents

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from agno.agent import Agent
 from agno.models.openai import OpenAIChat
 from agno.tools.arxiv import ArxivTools
+from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.tools.googlesearch import GoogleSearchTools
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -24,8 +26,22 @@ arxiv_research_agent = Agent(
     id="arxiv_research_agent",
     name="Arxiv Research Agent",
     model=model,
+    role="Arxiv Research Assistant",
     instructions="You are an expert research assistant specialized in finding and summarizing academic papers. Use the ArxivTools to search for papers based on user queries and provide concise summaries of the findings.",
     tools=[ArxivTools(download_dir=dir_path)],
+    add_datetime_to_context=True,
 )
+
+# define the websearch agent
+websearch_agent = Agent(
+    id="websearch_agent",
+    name="Web Search Agent",
+    model=model,
+    role="Web Search Assistant",
+    instructions="You are an expert research assistant specialized in finding information from the web. Use web search tools to find relevant articles and summarize the key points for the user.and also provide links to the sources you used.",
+    tools=[DuckDuckGoTools(), GoogleSearchTools()],
+    add_datetime_to_context=True,
+)
+
 
 
